df8d31182a98dc3f9fc2d3cfd1226a6979d8143e
Maria Konova <arim.novak@gmail.com>
2023-02-16 23:04:08 +0000

    adds welcome screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//import Header from "./Header";
+import "./Home.css";
 
 function Home() {
-
-    return (
-        <>
-            <h1>Make Up Home-Page</h1>
-
-
-        </>
-
+  const [charData, setCharData] = useState(null);
+  const brands = ["nyx", "maybelline", "dior", "glossier", "l'oreal"];
+  let randomBrand = brands[Math.floor(Math.random() * brands.length)];
+  //   const productTypes = [
+  //     "blush",
+  //     "bronzer",
+  //     "eyebrow",
+  //     "eyeliner",
+  //     "eyeshadow",
+  //     "foundation",
+  //     "lip_liner",
+  //     "lipstick",
+  //     "mascara",
+  //     "nail_polish",
+  //   ];
+  //   let randomProductType =
+  //     productTypes[Math.floor(Math.random() * productTypes.length)];
+  useEffect(() => {
+    fetch(
+      `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${randomBrand}`
     )
-}
+      .then((res) => res.json())
+      .then((data) => {
+        setCharData(data);
+        console.log(data);
+      });
+  }, []);
 
+  return (
+    <>
+      <div className="welcome">
+        <h1>Welcome!</h1>
+      </div>
+      <div>
+        <button>Search</button>
+      </div>
+      <div>Check out these products</div>
+      <div>{JSON.stringify(charData)}</div>
+    </>
+  );
+}
 
 export default Home;
