589a8a4f2c2041bccd2f4153e36ec28a1f562345
Nanda Nippani <nandanippani@yahoo.com>
2023-02-17 21:33:08 +0000

    product display and styling

--- src/components/ProductGallery.js
+++ src/components/ProductGallery.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Product from './Product';
+import './ProductGallery.css';
 
 
 const ProductGallery = () => {
     const [productData, setProductData] = useState(null);
 
     const fetchProductData = () => {
-        fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeshadow`)
+        fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=nail_polish`)
             .then(res => res.json())
             .then(data => {
-                let products = data.slice(0, 30);
+                let products = data.slice(14, 30);
                 console.log(products);
                 setProductData(products);
             });
@@ -25,15 +26,13 @@ const ProductGallery = () => {
 
 
     return (
-        <div>
-            {productData.length > 0 && (
-                <div>
-                    {
-                        productData.map(product => (
-                            <Product key={product.id} product={product} />
-                        ))}
-                </div>)}
+        <div className="product-gallery">
+            {
+                productData.map(product => (
+                    <Product key={product.id} product={product} />
+                ))}
         </div>
+
     )
 }
 
