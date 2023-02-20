5287f9bce0960b1b1ac32a8ee8f479782041f59d
Maria Konova <arim.novak@gmail.com>
2023-02-20 08:46:13 +0000

    adds page logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
-//import Header from "./Header";
+import Header from "./Header";
 import "./Home.css";
+import Product from "./Product";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [charData, setCharData] = useState(null);
+  const [listItems, setListItems] = useState(null);
+
   const brands = ["nyx", "maybelline", "dior", "glossier", "l'oreal"];
   let randomBrand = brands[Math.floor(Math.random() * brands.length)];
   //   const productTypes = [
@@ -26,21 +30,52 @@ function Home() {
     )
       .then((res) => res.json())
       .then((data) => {
-        setCharData(data);
-        console.log(data);
+        let info = data.slice(0, 4);
+        setCharData(info);
+        console.log(info);
       });
   }, []);
 
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `./main`;
+    navigate(path);
+  };
+  if (charData === null) {
+    return null;
+  }
   return (
     <>
+      <Header />
       <div className="welcome">
-        <h1>Welcome!</h1>
+        <h1 id="discover">Discover new make-up with Make Up App</h1>
       </div>
       <div>
-        <button>Search</button>
+        <button id="search" onClick={routeChange}>
+          Search for Products
+        </button>
+        <p id="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non leo
+          at justo facilisis bibendum non vel erat. Quisque sed imperdiet erat,
+          laoreet tempus ante. Fusce mi augue, tempor eu eros nec, sodales
+          interdum tortor. Proin consequat dui leo, ullamcorper suscipit mauris
+          blandit vitae. Ut rutrum consectetur diam, et auctor lacus fermentum
+          nec. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Nulla et orci non tellus placerat hendrerit nec sit amet libero.
+          Vivamus iaculis risus sit amet porta egestas. Duis sit amet eros a mi
+          egestas accumsan sit amet quis ex. Mauris sollicitudin neque sit amet
+          metus suscipit rutrum. Duis pellentesque purus in augue congue
+          vestibulum.
+        </p>
       </div>
-      <div>Check out these products</div>
-      <div>{JSON.stringify(charData)}</div>
+      <div>
+        <h2 id="check">Check out these products</h2>
+      </div>
+      <section>
+        {charData.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </section>
     </>
   );
 }
