cd858cb59804fe02414509b500cb451b4eb2492d
Maria Konova <arim.novak@gmail.com>
2023-02-20 22:52:53 +0000

    adds checkout logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,50 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
 function Header() {
-    return (
-        <div>
-            <header>
-                <h1>Make Up App</h1>
-                <nav>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/Main">Main</NavLink>
-                </nav>
-            </header>
-        </div>
-    )
-}
+  const [listOfProductIDs, setListOfProductIDs] = useState([]);
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const showCheckout = () => {
+    const checkoutList = JSON.parse(localStorage.getItem("checkoutList")) ?? [];
+    setListOfProductIDs(checkoutList);
+    handleShow();
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Make Up App</h1>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Main">Main</NavLink>
+        </nav>
+        <button onClick={showCheckout}>Check out</button>
+      </header>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        onShow={handleShow}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul>
+            <li>asdsa</li>
+            <li>qwewewq</li>
+          </ul>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
 
 export default Header;
